a1971d86f5fe8a67f00d70ff529e0d9372480384
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-07-21 20:31:52 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# Repository Template
-
-This is a repository template for new repositories.
-
-This file should be replaced by your own.
-
+# LmcUSer for Mezzio
 
---
+++ src/ConfigProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lmc\User\Mezzio;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => $this->getDependencies(),
+        ];
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            'factories' => [],
+        ];
+    }
+}

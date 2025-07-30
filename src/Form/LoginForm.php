bfd5188f29f60896346348a3faaf6bd37ae1495e
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2025-07-29 21:49:49 -0400

    Fixed form to use button element

--- src/Form/LoginForm.php
+++ src/Form/LoginForm.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Lmc\User\Mezzio\Form;
 
+use Laminas\Form\Element\Button;
 use Laminas\Form\Element\Password;
-use Laminas\Form\Element\Submit;
 use Laminas\Form\Element\Text;
 use Laminas\Form\Form;
 use Lmc\User\Mezzio\Options\Options;
@@ -36,10 +36,13 @@ class LoginForm extends Form
         ]);
         $this->add([
             'name'       => 'submit',
-            'type'       => Submit::class,
+            'type'       => Button::class,
             'options'    => [
                 'label' => 'Login',
-            ]
+            ],
+            'attributes' => [
+                'type' => 'submit',
+            ],
         ], [
             'priority' => -100,
         ]);
